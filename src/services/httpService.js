81a0f5a73a9ea827bc13f223abe98a6b288508c3
Asem Naderi <dev.asem.naderi@gmail.com>
2023-02-12 00:27:22 +0330

    set axios interceptors for http requsests

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -2,11 +2,33 @@ import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3005";
 
+axios.interceptors.request.use(
+    (response) => {
+        console.log(response);
+        return response;
+    },
+    (error) => {
+        console.log(error);
+        return Promise.reject();
+    }
+);
+
+axios.interceptors.response.use(
+    (response) => {
+        console.log(response);
+        return response;
+    },
+    (error) => {
+        console.log(error);
+        return Promise.reject();
+    }
+);
+
 const http = {
     get: axios.get,
     post: axios.post,
     delete: axios.delete,
-    put:axios.put
+    put: axios.put
 }
 
 export default http;
